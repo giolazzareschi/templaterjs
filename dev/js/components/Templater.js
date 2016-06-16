b5708740b29aa6c9691163b5b9f2ac0fa1639255
Giordano Bruno Lazzareschi <giordano.lazzareschi@softexpert.com>
2016-06-16 14:03:47 -0300

    First version

---
+++ dev/js/components/Templater.js
@@ -0,0 +1,174 @@
+var Templater = Base.extend({
+
+	template : '',
+
+	template_data : {},
+
+	child_template : undefined,
+
+	dom : undefined,
+
+	hasChild : false,
+
+	events : {},
+
+	buffer_rollback : undefined,
+
+	parent : undefined,
+
+	ajax : undefined,
+
+	model : undefined,
+
+	constructor : function( args ){
+
+		if( args && args.model !== undefined ){
+			args.model.owner = this;
+			this.ajax = new Ajax( args.model );
+		}else if( this.model !== undefined ){
+			this.model.owner = this;
+			this.ajax = new Ajax( this.model );
+		}
+
+		if( args && args.template !== undefined && args.template !== '' )
+			this.template = args.template;
+
+		if( args && args.template_data !== undefined )
+			this.template_data = args.template_data;
+
+		if( args && args.events !== undefined )
+			this.events = args.events;
+
+		if( args && args.parent !== undefined )
+			this.parent = args.parent;
+
+		if( this.template !== '' )
+			this.update_dom();
+	},
+
+	server_get : function(){		
+		this.ajax.get__();
+	},
+
+	hbs : function(){
+		this.update_child_template();
+
+		return Handlebars.compile( this.template )( this.template_data );
+	},
+
+	binds : function(){
+		
+	},
+
+	hide : function(){
+		this.dom.classList.add("hide");
+	},
+
+	show : function(){
+		this.dom.classList.remove("hide");
+	},
+
+	update_dom : function( rollback_dom ){
+		if( !rollback_dom ){
+			var dom = document.createElement('div');
+			dom.innerHTML = this.hbs();
+			this.dom = dom.children[0];
+		}else{
+			this.dom = rollback_dom;
+			this.buffer_rollback = undefined;
+		}
+
+		this.get_template_elements();
+
+		for( p in this.events )
+			this.register_events(p, this.events[p]);
+
+		this.binds();
+	},
+
+	update_child_template : function(){
+		if( this.child_template !== undefined )
+			for( tpl in this.child_template )
+				this.template_data[ tpl ] = Handlebars.compile(this.child_template[ tpl ])(this.template_data);		
+	},
+
+	get_dom : function(){
+		var dom = document.createElement('div');
+
+		dom.innerHTML = this.hbs();
+
+		return dom.children[0];
+	},
+
+	get_template_elements : function( dom ){
+		if( this.dom ){
+			var dom = dom !== undefined ? dom : this.dom, elements = dom.querySelectorAll('*[id]'), i=0, qt = elements.length;			
+			for( ; i < qt; i++ ){
+				var element = elements[ i ];
+				if( this.elements === undefined )
+					this.elements = {};
+
+				try{
+					this.elements[ element.id ] = element;
+				}catch(e){};
+			}
+		}
+	},
+
+	register_events : function(property, fn, dom){
+		var data = property.split(' '), event = data[0], selector = data.splice(1).join(' ').trim(), dom = dom !== undefined ? dom : this.dom;
+		try{
+			dom.querySelector( selector ).addEventListener(event, fn.bind(this), !1);
+		}catch(e){
+			console.log( selector );
+			console.log( e );
+		};
+	},
+
+	render : function( receiver ){
+		this.receiver = receiver;
+		receiver.innerHTML = "";
+		receiver.appendChild( this.dom );
+	},
+
+	append : function( receiver ){
+		this.receiver = receiver;		
+		receiver.appendChild( this.dom );
+	},
+
+	clear : function( receiver ){
+		this.receiver = receiver;	
+		this.receiver.innerHTML = "";
+	},
+
+	repaint : function(){
+		this.buffer_rollback = this.dom.cloneNode(true);
+		var temp_dom = this.get_dom();
+		this.dom.parentNode.replaceChild( temp_dom, this.dom );
+		this.dom = temp_dom;
+		this.get_template_elements();
+
+		for( p in this.events )
+			this.register_events(p, this.events[p], this.dom);
+
+		this.binds();		
+	},
+
+	rollback : function(){
+		if( this.buffer_rollback ){
+			this.update_dom( this.buffer_rollback );
+			this.receiver.innerHTML = "";
+			this.receiver.appendChild( this.dom );
+		}
+	},
+
+	remove : function(){
+		this.dom.parentNode.removeChild( this.dom );		
+	},
+
+	destroy : function(){
+		this.dom.parentNode.removeChild( this.dom );
+		delete this;
+	}
+
+});
